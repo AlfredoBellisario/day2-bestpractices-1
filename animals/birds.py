f63f456bed223bfe349f15359a62034b898e365d
Alfredo Bellisario <bellisarioalfredo@gmail.com>
2020-03-24 23:38:08 +0100

    Removed files

--- animals/birds.py
+++
@@ -1,9 +0,0 @@
-class Birds:
-    def __init__(self):
-        '''Constructor for Birds.'''
-        self.members = ['Canary', 'Turkey', 'Chicken']
-
-    def printMembers(self):
-        print('Printing members')
-        for member in self.members:
-            print('\t {}'.format(member))
--- animals/mammals.py
+++
@@ -1,8 +0,0 @@
-class Mammals:
-    def __init__(self):
-        '''Constructor for Mammals.'''
-        self.members = ['Zebra', 'Platypus', 'Dolphin']
-    def printMembers(self):
-        print('Printing members')
-        for member in self.members:
-            print('\t {}'.format(member))
